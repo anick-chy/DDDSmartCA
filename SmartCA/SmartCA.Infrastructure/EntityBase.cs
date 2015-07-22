9b0ec57728fb71792f23c7d8de90bd0f020415cc
anick-chy <anickchowdhury@gmail.com>
2015-07-22 18:13:43 +0600

    Entity base class for all domain entity.
    This behavior is necessary for comparing, sorting, and matching entity
    objects. This is nice because this plumbing  type of code is
    encapsulated in the infrastructure layer and keeps the domain layer 's
    entity
    objects free from these distractions.

--- SmartCA/SmartCA.Infrastructure/EntityBase.cs
+++ SmartCA/SmartCA.Infrastructure/EntityBase.cs
@@ -9,7 +9,8 @@ namespace SmartCA.Infrastructure.DomainBase
         /// <summary>
         /// Default Constructor
         /// </summary>
-        protected EntityBase() : this(null)
+        protected EntityBase()
+            : this(null)
         {
 
         }
@@ -34,5 +35,74 @@ namespace SmartCA.Infrastructure.DomainBase
         {
             get { return this.key; }
         }
+
+        #region equality test
+
+        /// <summary>
+        /// Determines whether the specified entity is equal to the
+        /// current instance.
+        /// </summary>
+        /// <param name="entity">An <see cref="System.Object" /> that
+        /// will be compared to the current instance.</param>
+        /// <returns>> True if the passed in entity is equal to the
+        /// current instance.</returns>
+        public override bool Equals(object entity)
+        {
+            if (entity == null || !(entity is EntityBase))
+            {
+                return false;
+            }
+            return base.Equals(entity);
+        }
+
+        /// <summary>
+        /// Operator overload for determining equality
+        /// </summary>
+        /// <param name="entity1">The first instance of an <see cref="EntityBase"/>.</param>
+        /// <param name="entity2">The second instance of an <see cref="EntityBase"/>.</param>
+        /// <returns>True if equal</returns>
+        public static bool operator ==(EntityBase entity1, EntityBase entity2)
+        {
+            //check for both null
+            if ((object)entity1 == null && (object)entity2 == null)
+            {
+                return true;
+            }
+
+            //check for either of them equals to null
+            if ((object)entity1 == null || (object)entity2 == null)
+            {
+                return false;
+            }
+
+            if (entity1.Key != entity2.Key)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Operator overload for determining inequality
+        /// </summary>
+        /// <param name="entity1">The first instance of an <see cref="EntityBase"/>.</param>
+        /// <param name="entity2">The second instance of an <see cref="EntityBase"/>.</param>
+        /// <returns></returns>
+        public static bool operator !=(EntityBase entity1, EntityBase entity2)
+        {
+            return (!(entity1 == entity2));
+        }
+
+        /// <summary>
+        /// Serves as a hash function for this type.
+        /// </summary>
+        /// <returns>A hash code for the current Key property</returns>
+        public override int GetHashCode()
+        {
+            return this.key.GetHashCode();
+        }
+
+        #endregion
     }
 }
