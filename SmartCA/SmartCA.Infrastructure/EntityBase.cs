27013b5bea1a58d2ae01239cfee1764b40112d00
anick-chy <anickchowdhury@gmail.com>
2015-07-22 17:47:12 +0600

    Initial Commit
    All files are created initially.

---
+++ SmartCA/SmartCA.Infrastructure/EntityBase.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace SmartCA.Infrastructure.DomainBase
+{
+    public abstract class EntityBase
+    {
+        private object key;
+
+        /// <summary>
+        /// Default Constructor
+        /// </summary>
+        protected EntityBase() : this(null)
+        {
+
+        }
+
+        /// <summary>
+        /// Overloaded constructor.
+        /// </summary>
+        /// <param name="key">An <see cref="System.Object" /> that
+        /// represents the primary identifier value for the
+        /// class.</param>
+        protected EntityBase(object key)
+        {
+            this.key = key;
+        }
+
+        /// <summary>
+        /// An <see cref="System.Object" /> that
+        /// represents the primary identifier value for the
+        /// class.
+        /// </summary>
+        public object Key
+        {
+            get { return this.key; }
+        }
+    }
+}
