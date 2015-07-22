b4905904ed77f4e5ca14246ab14ffb5d43b7a6ca
anick-chy <anickchowdhury@gmail.com>
2015-07-22 19:27:30 +0600

    Repository

---
+++ SmartCA/SmartCA.Infrastructure/IRepository.cs
@@ -0,0 +1,9 @@
+﻿using SmartCA.Infrastructure.DomainBase;
+using System;
+
+namespace SmartCA.Infrastructure.RepositoryFramework
+{
+    public interface IRepository<T> where T : EntityBase
+    {
+    }
+}
