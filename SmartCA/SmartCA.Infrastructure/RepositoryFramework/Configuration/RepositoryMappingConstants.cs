10ac1634a85ee890efbb2cce7e515a0ad11e9aad
anick-chy <anickchowdhury@gmail.com>
2015-08-17 16:45:54 +0600

    Repository Factory Setup
    Changes in repository factory and app config.

---
+++ SmartCA/SmartCA.Infrastructure/RepositoryFramework/Configuration/RepositoryMappingConstants.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartCA.Infrastructure.RepositoryFramework.Configuration
+{
+    internal class RepositoryMappingConstants
+    {
+        public const string ConfigurationPropertyName = "repositroyMappings";
+        public const string ConfigurationElementName = "repositroyMapping";
+        public const string InterfaceShortTypeNameAttributeName = "interfaceShortTypeName";
+        public const string RepositoryFullTypeNameAttributeName = "repositoryFullTypeName";
+        public const string RepositoryMappingsConfigurationSectionName = "repositoryMappingsConfiguration";
+    }
+}
---
+++ SmartCA/SmartCA.Infrastructure/RepositoryFramework/Configuration/RepositorySettings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Configuration;
+
+
+namespace SmartCA.Infrastructure.RepositoryFramework.Configuration
+{
+    class RepositorySettings : ConfigurationSection
+    {
+        [ConfigurationProperty(RepositoryMappingConstants.ConfigurationPropertyName, IsDefaultCollection = true)]
+        public RepositoryMappingCollection RespositoryMappings
+        {
+            get { return (RepositoryMappingCollection)base[RepositoryMappingConstants.ConfigurationPropertyName]; }
+        }
+    }
+}
